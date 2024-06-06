a161870ed449aca9413a999f45188973a51d98d2
Sum Bui <buingocsum@gmail.com>
2024-06-06 19:07:54 +0700

    add guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('hello')
 export class UserController {
   constructor(private userService: UserService) {}
