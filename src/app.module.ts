c8c4269da8da1421195bc2299bdfe21a38a2b508
Sum Bui <buingocsum@gmail.com>
2024-06-06 19:12:06 +0700

    change folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { GuardModule } from './guard/guard.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, GuardModule],
 })
 export class AppModule {}
